8907dff57a1d616d10c310060e1c1429735ddf8f
janellca <janellca@student.21-school.ru>
2024-03-08 16:21:06 +0700

    2

--- js/index.js
+++ js/index.js
@@ -1,12 +1,19 @@
 window.onscroll = function() {stickyMessageBar()};
 
+var sendMessage = document.getElementById("sendMessage");
 var message = document.getElementById("message");
 var sticky = message.offsetTop;
 
 function stickyMessageBar() {
     if (window.scrollY >= sticky) {
         message.classList.add("sticky");
+        sendMessage.classList.add("sticky");
       } else {
         message.classList.remove("sticky");
+        sendMessage.classList.remove("sticky");
       }
     }
+
+function sendMessage() {
+
+}
